2877814b2027839f85f7e21eb9701f260e666902
Ryosuke Ito <rito.0305@gmail.com>
2019-08-11 23:20:53 +0900

    Bump version to 0.3.1

--- Sources/XCNMacroDefinitions.h
+++ Sources/XCNMacroDefinitions.h
@@ -9,7 +9,7 @@
 #ifndef XCNMacroDefinitions_h
 #define XCNMacroDefinitions_h
 
-#define XCN_PROGRAM_VERSION "0.3.0"
+#define XCN_PROGRAM_VERSION "0.3.1"
 
 #ifdef __cplusplus
 #define XCN_EXTERN extern "C"
