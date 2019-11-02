a5c79d93b814dcd1e26d6c14ae508157c2fd8632
Ryosuke Ito <rito.0305@gmail.com>
2019-11-02 21:04:53 +0900

    Invert arguments of assertions
    Like [Apple does](https://github.com/apple/swift-driver/blob/e1e26c382a3a961cee6ce56a91554ed95473131f/Tests/SwiftDriverTests/TripleTests.swift#L18).
    
    ```python
    import fileinput
    import glob
    import re
    
    assertion_re =
    re.compile(r'^(.*)XCTAssert(|Not)Equal(|Objects)\(([^,]+),\s*(.+)\)(.*)$')
    files = glob.glob('./Tests/*.m')
    
    for line in fileinput.input(files, inplace=True):
        matched = assertion_re.search(line)
        if matched is not None:
            print('{0}XCTAssert{1}Equal{2}({4},
    {3}){5}'.format(*matched.groups()))
        else:
            print(line.rstrip())
    ```

--- Tests/XCNErrorsTests.m
+++ Tests/XCNErrorsTests.m
@@ -16,42 +16,42 @@
 
 - (void)testXCNFileWriteUnknownErrorCreateWithPath {
     NSError *error = XCNFileWriteUnknownErrorCreateWithPath(@"/path/to/file");
-    XCTAssertEqualObjects(XCNErrorDomain, error.domain);
-    XCTAssertEqual(1, error.code);
-    XCTAssertEqualObjects(@"Cannot write at path '/path/to/file'.", error.localizedDescription);
+    XCTAssertEqualObjects(error.domain, XCNErrorDomain);
+    XCTAssertEqual(error.code, 1);
+    XCTAssertEqualObjects(error.localizedDescription, @"Cannot write at path '/path/to/file'.");
     XCTAssertNil(error.localizedFailureReason);
 }
 
 - (void)testXCNIDEFoundationInconsistencyErrorCreateWithFormat {
     NSError *error = XCNIDEFoundationInconsistencyErrorCreateWithFormat(@"%@ %@ %@", @"foo", @"bar", @"baz");
-    XCTAssertEqualObjects(XCNErrorDomain, error.domain);
-    XCTAssertEqual(100, error.code);
-    XCTAssertEqualObjects(@"foo bar baz", error.localizedDescription);
-    XCTAssertEqualObjects(@"This error means Xcode changes interface to manipulate project files.", error.localizedFailureReason);
+    XCTAssertEqualObjects(error.domain, XCNErrorDomain);
+    XCTAssertEqual(error.code, 100);
+    XCTAssertEqualObjects(error.localizedDescription, @"foo bar baz");
+    XCTAssertEqualObjects(error.localizedFailureReason, @"This error means Xcode changes interface to manipulate project files.");
 }
 
 - (void)testXCNInvalidArgumentErrorCreateWithShortOption {
     NSError *error = XCNInvalidArgumentErrorCreateWithShortOption('X');
-    XCTAssertEqualObjects(XCNErrorDomain, error.domain);
-    XCTAssertEqual(110, error.code);
-    XCTAssertEqualObjects(@"Unrecognized option '-X'.", error.localizedDescription);
+    XCTAssertEqualObjects(error.domain, XCNErrorDomain);
+    XCTAssertEqual(error.code, 110);
+    XCTAssertEqualObjects(error.localizedDescription, @"Unrecognized option '-X'.");
     XCTAssertNil(error.localizedFailureReason);
 }
 
 - (void)testXCNInvalidArgumentErrorCreateWithLongOption {
     NSError *error = XCNInvalidArgumentErrorCreateWithLongOption("--invalid");
-    XCTAssertEqualObjects(XCNErrorDomain, error.domain);
-    XCTAssertEqual(110, error.code);
-    XCTAssertEqualObjects(@"Unrecognized option '--invalid'.", error.localizedDescription);
+    XCTAssertEqualObjects(error.domain, XCNErrorDomain);
+    XCTAssertEqual(error.code, 110);
+    XCTAssertEqualObjects(error.localizedDescription, @"Unrecognized option '--invalid'.");
     XCTAssertNil(error.localizedFailureReason);
 }
 
 - (void)testXCNWrongNumberOfArgumentsErrorCreateWithRange {
     NSRange acceptableRangeOfArgumentsCount = NSMakeRange(1, 1);
     NSError *error = XCNWrongNumberOfArgumentsErrorCreateWithRange(acceptableRangeOfArgumentsCount, 4);
-    XCTAssertEqualObjects(XCNErrorDomain, error.domain);
-    XCTAssertEqual(111, error.code);
-    XCTAssertEqualObjects(@"Wrong number of arguments (4 for 1..2).", error.localizedDescription);
+    XCTAssertEqualObjects(error.domain, XCNErrorDomain);
+    XCTAssertEqual(error.code, 111);
+    XCTAssertEqualObjects(error.localizedDescription, @"Wrong number of arguments (4 for 1..2).");
     XCTAssertNil(error.localizedFailureReason);
 }
 
