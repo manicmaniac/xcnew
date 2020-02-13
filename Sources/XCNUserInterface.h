5cfcf367981d1cec0d556240d072ca11ced4a45d
Ryosuke Ito <rito.0305@gmail.com>
2020-02-13 20:18:46 +0900

    Add enum for user interface

---
+++ Sources/XCNUserInterface.h
@@ -0,0 +1,14 @@
+//
+//  XCNUserInterface.h
+//  xcnew
+//
+//  Created by Ryosuke Ito on 2/13/20.
+//  Copyright © 2020 Ryosuke Ito. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSUInteger, XCNUserInterface) {
+    XCNUserInterfaceSwiftUI,
+    XCNUserInterfaceStoryboard,
+};
