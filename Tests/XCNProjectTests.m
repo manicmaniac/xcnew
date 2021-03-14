4df4126a6b54cd31e126a351a122f1fc368cb08b
Ryosuke Ito <rito.0305@gmail.com>
2021-03-14 21:52:32 +0900

    Add a fundamental test for XCNProject
    The command line tool `xctest`, which runs test bundles has its own
    rpath for linking dynamic frameworks and libraries.  However its rpath
    differs from `xcnew` binary's one so unfortunately `IDEInitialize()`
    fails to load Xcode private frameworks.  To avoid this situation, I
    needed to add environment variables to the scheme in order to let
    `xctest` know the correct path of private frameworks and libraries.

---
+++ Tests/XCNProjectTests.m
@@ -0,0 +1,48 @@
+//
+//  XCNProjectTests.m
+//  xcnew-tests
+//
+//  Created by Ryosuke Ito on 3/13/21.
+//  Copyright © 2021 Ryosuke Ito. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "XCNProject.h"
+
+@interface XCNProjectTests : XCTestCase
+@end
+
+@implementation XCNProjectTests {
+    NSFileManager *_fileManager;
+    NSURL *_temporaryDirectoryURL;
+}
+
+- (void)setUp {
+    _fileManager = NSFileManager.defaultManager;
+    NSError *error;
+    _temporaryDirectoryURL = [_fileManager URLForDirectory:NSItemReplacementDirectory
+                                                  inDomain:NSUserDomainMask
+                                         appropriateForURL:_fileManager.temporaryDirectory
+                                                    create:YES
+                                                     error:&error];
+    if (!_temporaryDirectoryURL) {
+        [self setContinueAfterFailure:NO];
+        XCTFail(@"%@", error);
+    }
+}
+
+- (void)tearDown {
+    NSError *error;
+    if (![_fileManager removeItemAtURL:_temporaryDirectoryURL error:&error]) {
+        XCTFail(@"%@", error);
+    }
+}
+
+- (void)testWriteToURL {
+    XCNProject *project = [[XCNProject alloc] initWithProductName:@"Product"];
+    NSError *error = nil;
+    XCTAssertTrue([project writeToURL:_temporaryDirectoryURL error:&error]);
+    XCTAssertNil(error);
+}
+
+@end
