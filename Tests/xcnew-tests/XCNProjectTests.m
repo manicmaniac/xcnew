827e86ac654c25274c86b8f20eca7d83b0327521
Ryosuke Ito <rito.0305@gmail.com>
2021-03-23 00:52:54 +0900

    Add test cases for XCNProject

--- Tests/xcnew-tests/XCNProjectTests.m
+++ Tests/xcnew-tests/XCNProjectTests.m
@@ -8,14 +8,23 @@
 
 #import <XCTest/XCTest.h>
 #import "XCNProject.h"
+#import "XCNMacroDefinitions.h"
 
 @interface XCNProjectTests : XCTestCase
+
+@property (nonatomic, readonly) NSFileWrapper *fileWrapper;
+
 @end
 
 @implementation XCNProjectTests {
     NSFileManager *_fileManager;
     NSURL *_temporaryDirectoryURL;
+    XCNProject *_project;
+    NSURL *_url;
+    NSFileWrapper *_fileWrapper;
 }
+
+static NSString *const kProductName = @"Example";
 
 - (void)setUp {
     _fileManager = NSFileManager.defaultManager;
@@ -27,8 +36,10 @@
                                                      error:&error];
     if (!_temporaryDirectoryURL) {
         [self setContinueAfterFailure:NO];
-        XCTFail(@"%@", error);
+        return XCTFail(@"%@", error);
     }
+    _project = [[XCNProject alloc] initWithProductName:kProductName];
+    _url = [_temporaryDirectoryURL URLByAppendingPathComponent:kProductName];
 }
 
 - (void)tearDown {
@@ -38,11 +49,194 @@
     }
 }
 
-- (void)testWriteToURL {
-    XCNProject *project = [[XCNProject alloc] initWithProductName:@"Product"];
-    NSError *error = nil;
-    XCTAssertTrue([project writeToURL:_temporaryDirectoryURL timeout:60 error:&error]);
+- (void)testWriteToURLWithDefaultProperties {
+    NSError *error;
+    XCTAssertTrue([_project writeToURL:_url timeout:10 error:&error]);
     XCTAssertNil(error);
+    XCTAssertTrue(self.fileWrapper.fileWrappers[@"Example.xcodeproj"].fileWrappers[@"project.pbxproj"].isRegularFile);
+    XCTAssertTrue(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"Info.plist"].isRegularFile);
+    NSFileWrapper *appDelegateFileWrapper = self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"AppDelegate.swift"];
+    NSString *appDelegateContents = [[NSString alloc] initWithData:appDelegateFileWrapper.regularFileContents encoding:NSUTF8StringEncoding];
+    XCTAssertTrue([appDelegateContents containsString:@"Example"]);
+    XCTAssertTrue(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"ContentView.swift"].isRegularFile);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"Base.lproj"].fileWrappers[@"Main.storyboard"]);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@".git"]);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"Example.xcdatamodeld"]);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"ExampleTests"]);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"ExampleUITests"]);
+
+}
+
+- (void)testWriteToURLWithOrganizationName {
+    _project.organizationName = @"Organization";
+    NSError *error;
+    XCTAssertTrue([_project writeToURL:_url timeout:10 error:&error]);
+    XCTAssertNil(error);
+    XCTAssertTrue(self.fileWrapper.fileWrappers[@"Example.xcodeproj"].fileWrappers[@"project.pbxproj"].isRegularFile);
+    XCTAssertTrue(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"Info.plist"].isRegularFile);
+    NSFileWrapper *appDelegateFileWrapper = self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"AppDelegate.swift"];
+    NSString *appDelegateContents = [[NSString alloc] initWithData:appDelegateFileWrapper.regularFileContents encoding:NSUTF8StringEncoding];
+    XCTAssertTrue([appDelegateContents containsString:@"Example"]);
+    XCTAssertTrue([appDelegateContents containsString:@"Organization"]);
+    XCTAssertTrue(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"ContentView.swift"].isRegularFile);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"Base.lproj"].fileWrappers[@"Main.storyboard"]);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@".git"]);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"Example.xcdatamodeld"]);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"ExampleTests"]);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"ExampleUITests"]);
+}
+
+- (void)testWriteToURLWithOrganizationIdentifier {
+    static NSString *const organizationIdentifier = @"com.example.organization";
+    _project.organizationIdentifier = organizationIdentifier;
+    NSError *error;
+    XCTAssertTrue([_project writeToURL:_url timeout:10 error:&error]);
+    XCTAssertNil(error);
+    NSFileWrapper *projectFileWrapper = self.fileWrapper.fileWrappers[@"Example.xcodeproj"].fileWrappers[@"project.pbxproj"];
+    NSString *projectContents = [[NSString alloc] initWithData:projectFileWrapper.regularFileContents encoding:NSUTF8StringEncoding];
+    XCTAssertTrue([projectContents containsString:organizationIdentifier]);
+    XCTAssertTrue(self.fileWrapper.fileWrappers[@"Example.xcodeproj"].fileWrappers[@"project.pbxproj"].isRegularFile);
+    XCTAssertTrue(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"Info.plist"].isRegularFile);
+    NSFileWrapper *appDelegateFileWrapper = self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"AppDelegate.swift"];
+    NSString *appDelegateContents = [[NSString alloc] initWithData:appDelegateFileWrapper.regularFileContents encoding:NSUTF8StringEncoding];
+    XCTAssertTrue([appDelegateContents containsString:@"Example"]);
+    XCTAssertTrue(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"ContentView.swift"].isRegularFile);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"Base.lproj"].fileWrappers[@"Main.storyboard"]);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@".git"]);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"Example.xcdatamodeld"]);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"ExampleTests"]);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"ExampleUITests"]);
+}
+
+- (void)testWriteToURLWithUnitTests {
+    _project.feature |= XCNProjectFeatureUnitTests;
+    NSError *error = nil;
+    XCTAssertTrue([_project writeToURL:_url timeout:10 error:&error]);
+    XCTAssertNil(error);
+    XCTAssertTrue(self.fileWrapper.fileWrappers[@"Example.xcodeproj"].fileWrappers[@"project.pbxproj"].isRegularFile);
+    XCTAssertTrue(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"Info.plist"].isRegularFile);
+    NSFileWrapper *appDelegateFileWrapper = self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"AppDelegate.swift"];
+    NSString *appDelegateContents = [[NSString alloc] initWithData:appDelegateFileWrapper.regularFileContents encoding:NSUTF8StringEncoding];
+    XCTAssertTrue([appDelegateContents containsString:@"Example"]);
+    XCTAssertTrue(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"ContentView.swift"].isRegularFile);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"Base.lproj"].fileWrappers[@"Main.storyboard"]);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@".git"]);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"Example.xcdatamodeld"]);
+    XCTAssertTrue(self.fileWrapper.fileWrappers[@"ExampleTests"].fileWrappers[@"Info.plist"].isRegularFile);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"ExampleUITests"]);
+}
+
+- (void)testWriteToURLWithUITests {
+    _project.feature |= XCNProjectFeatureUITests;
+    NSError *error = nil;
+    XCTAssertTrue([_project writeToURL:_url timeout:10 error:&error]);
+    XCTAssertNil(error);
+    XCTAssertTrue(self.fileWrapper.fileWrappers[@"Example.xcodeproj"].fileWrappers[@"project.pbxproj"].isRegularFile);
+    XCTAssertTrue(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"Info.plist"].isRegularFile);
+    NSFileWrapper *appDelegateFileWrapper = self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"AppDelegate.swift"];
+    NSString *appDelegateContents = [[NSString alloc] initWithData:appDelegateFileWrapper.regularFileContents encoding:NSUTF8StringEncoding];
+    XCTAssertTrue([appDelegateContents containsString:@"Example"]);
+    XCTAssertTrue(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"ContentView.swift"].isRegularFile);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"Base.lproj"].fileWrappers[@"Main.storyboard"]);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@".git"]);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"Example.xcdatamodeld"]);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"ExampleTests"]);
+    XCTAssertTrue(self.fileWrapper.fileWrappers[@"ExampleUITests"].fileWrappers[@"Info.plist"].isRegularFile);
+}
+
+- (void)testWriteToURLWithCoreData {
+    _project.feature |= XCNProjectFeatureCoreData;
+    NSError *error = nil;
+    XCTAssertTrue([_project writeToURL:_url timeout:10 error:&error]);
+    XCTAssertNil(error);
+    XCTAssertTrue(self.fileWrapper.fileWrappers[@"Example.xcodeproj"].fileWrappers[@"project.pbxproj"].isRegularFile);
+    XCTAssertTrue(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"Info.plist"].isRegularFile);
+    NSFileWrapper *appDelegateFileWrapper = self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"AppDelegate.swift"];
+    NSString *appDelegateContents = [[NSString alloc] initWithData:appDelegateFileWrapper.regularFileContents encoding:NSUTF8StringEncoding];
+    XCTAssertTrue([appDelegateContents containsString:@"Example"]);
+    XCTAssertTrue([appDelegateContents containsString:@"NSPersistentContainer"]);
+    XCTAssertFalse([appDelegateContents containsString:@"NSPersistentCloudKitContainer"]);
+    XCTAssertTrue(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"ContentView.swift"].isRegularFile);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"Base.lproj"].fileWrappers[@"Main.storyboard"]);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@".git"]);
+    XCTAssertTrue(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"Example.xcdatamodeld"].isDirectory);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"ExampleTests"]);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"ExampleUITests"]);
+}
+
+- (void)testWriteToURLWithCoreDataCloudKit {
+    _project.feature |= (XCNProjectFeatureCoreData | XCNProjectFeatureCloudKit);
+    NSError *error = nil;
+    XCTAssertTrue([_project writeToURL:_url timeout:10 error:&error]);
+    XCTAssertNil(error);
+    XCTAssertTrue(self.fileWrapper.fileWrappers[@"Example.xcodeproj"].fileWrappers[@"project.pbxproj"].isRegularFile);
+    XCTAssertTrue(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"Info.plist"].isRegularFile);
+    NSFileWrapper *appDelegateFileWrapper = self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"AppDelegate.swift"];
+    NSString *appDelegateContents = [[NSString alloc] initWithData:appDelegateFileWrapper.regularFileContents encoding:NSUTF8StringEncoding];
+    XCTAssertTrue([appDelegateContents containsString:@"Example"]);
+    XCTAssertFalse([appDelegateContents containsString:@"NSPersistentContainer"]);
+    XCTAssertTrue([appDelegateContents containsString:@"NSPersistentCloudKitContainer"]);
+    XCTAssertTrue(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"ContentView.swift"].isRegularFile);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"Base.lproj"].fileWrappers[@"Main.storyboard"]);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@".git"]);
+    XCTAssertTrue(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"Example.xcdatamodeld"].isDirectory);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"ExampleTests"]);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"ExampleUITests"]);
+}
+
+- (void)testWriteToURLWithObjectiveC {
+    _project.language = XCNLanguageObjectiveC;
+    NSError *error = nil;
+    XCTAssertTrue([_project writeToURL:_url timeout:10 error:&error]);
+    XCTAssertNil(error);
+    XCTAssertTrue(self.fileWrapper.fileWrappers[@"Example.xcodeproj"].fileWrappers[@"project.pbxproj"].isRegularFile);
+    XCTAssertTrue(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"Info.plist"].isRegularFile);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"AppDelegate.swift"]);
+    NSFileWrapper *appDelegateFileWrapper = self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"AppDelegate.m"];
+    NSString *appDelegateContents = [[NSString alloc] initWithData:appDelegateFileWrapper.regularFileContents encoding:NSUTF8StringEncoding];
+    XCTAssertTrue([appDelegateContents containsString:@"Example"]);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"ContentView.swift"]);
+    XCTAssertTrue(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"Base.lproj"].fileWrappers[@"Main.storyboard"].isRegularFile);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@".git"]);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"Example.xcdatamodeld"]);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"ExampleTests"]);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"ExampleUITests"]);
+}
+
+- (void)testWriteToURLWithStoryboard {
+    _project.userInterface = XCNUserInterfaceStoryboard;
+    NSError *error = nil;
+    XCTAssertTrue([_project writeToURL:_url timeout:10 error:&error]);
+    XCTAssertNil(error);
+    XCTAssertTrue(self.fileWrapper.fileWrappers[@"Example.xcodeproj"].fileWrappers[@"project.pbxproj"].isRegularFile);
+    XCTAssertTrue(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"Info.plist"].isRegularFile);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"AppDelegate.m"]);
+    NSFileWrapper *appDelegateFileWrapper = self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"AppDelegate.swift"];
+    NSString *appDelegateContents = [[NSString alloc] initWithData:appDelegateFileWrapper.regularFileContents encoding:NSUTF8StringEncoding];
+    XCTAssertTrue([appDelegateContents containsString:@"Example"]);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"ContentView.swift"]);
+    XCTAssertTrue(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"Base.lproj"].fileWrappers[@"Main.storyboard"].isRegularFile);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@".git"]);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"Example"].fileWrappers[@"Example.xcdatamodeld"]);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"ExampleTests"]);
+    XCTAssertNil(self.fileWrapper.fileWrappers[@"ExampleUITests"]);
+}
+
+- (NSFileWrapper *)fileWrapper {
+    @synchronized (self) {
+        if (!_fileWrapper) {
+            NSError *error;
+            _fileWrapper = [[NSFileWrapper alloc] initWithURL:_url
+                                                      options:(NSFileWrapperReadingOptions)0
+                                                        error:&error];
+            if (!_fileWrapper) {
+                self.continueAfterFailure = NO;
+                XCTFail(@"%@", error);
+                return nil;
+            }
+        }
+        return _fileWrapper;
+    }
 }
 
 @end
