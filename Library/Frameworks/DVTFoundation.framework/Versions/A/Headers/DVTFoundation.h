aa37c4c59a9ac28f0bfee2920dbc4e68fe7f8ed9
Ryosuke Ito <rito.0305@gmail.com>
2021-05-09 02:11:13 +0900

    Bundle stub of private frameworks

---
+++ Library/Frameworks/DVTFoundation.framework/Versions/A/Headers/DVTFoundation.h
@@ -0,0 +1,13 @@
+
+//
+//  DVTFoundation.h
+//  DVTFoundation
+//
+//  Created by Ryosuke Ito on 5/9/21.
+//  Copyright © 2021 Ryosuke Ito. All rights reserved.
+//
+
+#import <DVTFoundation/CDStructures.h>
+#import <DVTFoundation/DVTFilePath.h>
+#import <DVTFoundation/DVTFileSystemRepresentationProviding-Protocol.h>
+#import <DVTFoundation/DVTPlatform.h>
