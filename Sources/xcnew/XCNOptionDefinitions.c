34c303aa1cda4ed4612d3f9d90848f7bf2bc558b
Ryosuke Ito <rito.0305@gmail.com>
2022-08-07 12:37:47 +0900

    Regenerate source file and fix indent
    ./Scripts/xml2c -p XCN Sources/xcnew/xcnew.mxml Sources/xcnew/XCNOptionDefinitions.c

--- Sources/xcnew/XCNOptionDefinitions.c
+++ Sources/xcnew/XCNOptionDefinitions.c
@@ -6,24 +6,24 @@
 #include <getopt.h>
 
 const char XCNHelp[] = "xcnew - A command line interface to make a project for iOS Single View App.\n"
-                           "\n"
-                           "Usage: xcnew [-h|v] -i <ORG_ID> [-tcCosS] <PRODUCT_NAME> [OUTPUT_DIR]\n"
-                           "\n"
-                           "Options:\n"
-                           "    -h, --help                Show help and exit\n"
-                           "    -v, --version             Show version and exit\n"
-                           "    -i <ORG_ID>, --organization-identifier <ORG_ID>\n"
-                           "                              Specify organization's identifier\n"
-                           "    -t, --has-tests           Enable unit and UI tests\n"
-                           "    -c, --use-core-data       Enable Core Data template\n"
-                           "    -C, --use-cloud-kit       Enable Core Data with CloudKit template (overrides -c option)\n"
-                           "    -o, --objc                Use Objective-C instead of Swift (overridden by -s and -S options)\n"
-                           "    -s, --swift-ui            Use Swift UI instead of Storyboard\n"
-                           "    -S, --swift-ui-lifecycle  Use Swift UI lifecycle (overrides -s option)\n"
-                           "\n"
-                           "Arguments:\n"
-                           "    <PRODUCT_NAME>            Required TARGET_NAME of project.pbxproj\n"
-                           "    [OUTPUT_DIR]              Optional directory name of the project";
+                       "\n"
+                       "Usage: xcnew [-h|v] -i <ORG_ID> [-tcCosS] <PRODUCT_NAME> [OUTPUT_DIR]\n"
+                       "\n"
+                       "Options:\n"
+                       "    -h, --help                Show help and exit\n"
+                       "    -v, --version             Show version and exit\n"
+                       "    -i <ORG_ID>, --organization-identifier <ORG_ID>\n"
+                       "                              Specify organization's identifier\n"
+                       "    -t, --has-tests           Enable unit and UI tests\n"
+                       "    -c, --use-core-data       Enable Core Data template\n"
+                       "    -C, --use-cloud-kit       Enable Core Data with CloudKit template (overrides -c option)\n"
+                       "    -o, --objc                Use Objective-C instead of Swift (overridden by -s and -S options)\n"
+                       "    -s, --swift-ui            Use Swift UI instead of Storyboard\n"
+                       "    -S, --swift-ui-lifecycle  Use Swift UI lifecycle (overrides -s option)\n"
+                       "\n"
+                       "Arguments:\n"
+                       "    <PRODUCT_NAME>            Required TARGET_NAME of project.pbxproj\n"
+                       "    [OUTPUT_DIR]              Optional directory name of the project";
 
 const char XCNShortOptions[] = "hvi:tcCosS";
 
